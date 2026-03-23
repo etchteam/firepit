37cefd59ce9cc75c7655ddd08d8973ee0183c357
Annie Seaward <annie@etch.co>
2026-03-23 13:31:59 +0000

    test: use fixed consts for test labels

--- test/system/link_paste_test.rb
+++ test/system/link_paste_test.rb
@@ -1,46 +1,49 @@
 require "application_system_test_case"
 
 class LinkPasteTest < ApplicationSystemTestCase
+  LINK_TEXT = "Click here for more info"
+  EDITOR_LABEL = "Write a message"
+
   setup do
     sign_in "jz@37signals.com"
     join_room rooms(:designers)
   end
 
   test "pasting a URL over selected text creates a link" do
-    fill_in_rich_text_area "Write a message", with: "Click here for more info"
-    paste_into_editor("Write a message", text: "https://example.com", selection: [ 0, 10 ])
-    assert page.find(:rich_textarea, "Write a message").has_css?("a[href='https://example.com']")
+    fill_in_rich_text_area EDITOR_LABEL, with: LINK_TEXT
+    paste_into_editor(EDITOR_LABEL, text: "https://example.com", selection: [ 0, 10 ])
+    assert page.find(:rich_textarea, EDITOR_LABEL).has_css?("a[href='https://example.com']")
   end
 
   test "pasting a URL without a selection does not create a link" do
-    fill_in_rich_text_area "Write a message", with: "Hello"
-    paste_into_editor("Write a message", text: "https://example.com", selection: [ 5, 5 ])
-    assert page.find(:rich_textarea, "Write a message").has_no_css?("a")
+    fill_in_rich_text_area EDITOR_LABEL, with: "Hello"
+    paste_into_editor(EDITOR_LABEL, text: "https://example.com", selection: [ 5, 5 ])
+    assert page.find(:rich_textarea, EDITOR_LABEL).has_no_css?("a")
   end
 
   test "pasting a URL with embedded line breaks creates a link with the line breaks removed" do
-    fill_in_rich_text_area "Write a message", with: "Click here for more info"
-    paste_into_editor("Write a message", text: "https://app.frontapp.com/inboxes/teammates/\n10381066/inbox/all/56924074506", selection: [ 0, 10 ])
-    assert page.find(:rich_textarea, "Write a message").has_css?("a[href='https://app.frontapp.com/inboxes/teammates/10381066/inbox/all/56924074506']")
+    fill_in_rich_text_area EDITOR_LABEL, with: LINK_TEXT
+    paste_into_editor(EDITOR_LABEL, text: "https://app.frontapp.com/inboxes/teammates/\n10381066/inbox/all/56924074506", selection: [ 0, 10 ])
+    assert page.find(:rich_textarea, EDITOR_LABEL).has_css?("a[href='https://app.frontapp.com/inboxes/teammates/10381066/inbox/all/56924074506']")
   end
 
   test "pasting non-URL text over selected text does not create a link" do
-    fill_in_rich_text_area "Write a message", with: "Click here for more info"
-    paste_into_editor("Write a message", text: "just some text", selection: [ 0, 10 ])
-    assert page.find(:rich_textarea, "Write a message").has_no_css?("a")
+    fill_in_rich_text_area EDITOR_LABEL, with: LINK_TEXT
+    paste_into_editor(EDITOR_LABEL, text: "just some text", selection: [ 0, 10 ])
+    assert page.find(:rich_textarea, EDITOR_LABEL).has_no_css?("a")
   end
 
     test "pasting an incomplete URL over selected text does not create a link" do
-    fill_in_rich_text_area "Write a message", with: "Click here for more info"
-    paste_into_editor("Write a message", text: "www.google", selection: [ 0, 10 ])
-    assert page.find(:rich_textarea, "Write a message").has_no_css?("a")
+    fill_in_rich_text_area EDITOR_LABEL, with: LINK_TEXT
+    paste_into_editor(EDITOR_LABEL, text: "www.google", selection: [ 0, 10 ])
+    assert page.find(:rich_textarea, EDITOR_LABEL).has_no_css?("a")
   end
 
   test "pasting a hyperlink copied directly from a browser creates a link from the HTML href" do
-    fill_in_rich_text_area "Write a message", with: "Click here for more info"
-    paste_into_editor("Write a message", text: "Inbox", selection: [ 0, 10 ],
+    fill_in_rich_text_area EDITOR_LABEL, with: LINK_TEXT
+    paste_into_editor(EDITOR_LABEL, text: "Inbox", selection: [ 0, 10 ],
       html: "<a href='https://app.frontapp.com/inboxes/teammates/10381066/inbox/all/56924074506'>Inbox</a>")
-    assert page.find(:rich_textarea, "Write a message").has_css?("a[href='https://app.frontapp.com/inboxes/teammates/10381066/inbox/all/56924074506']")
+    assert page.find(:rich_textarea, EDITOR_LABEL).has_css?("a[href='https://app.frontapp.com/inboxes/teammates/10381066/inbox/all/56924074506']")
   end
 
   private
